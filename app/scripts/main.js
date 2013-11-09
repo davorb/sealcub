e4e702c5c5666e6b4bae6b81bee3b2d2b09830d0
Adam Wamai Egesa <adam.wamai@gmail.com>
2013-11-09 14:17:42 +0100

    Fixed indentation
    Fixed indentation

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,14 +1,14 @@
 require.config({
-    paths: {
-        jquery: '../bower_components/jquery/jquery',
-        requirejs: '../bower_components/requirejs/require',
-        underscore: '../bower_components/underscore/underscore'
-    }
+  paths: {
+    jquery: '../bower_components/jquery/jquery',
+    requirejs: '../bower_components/requirejs/require',
+    underscore: '../bower_components/underscore/underscore'
+  }
 });
 
 require(['app', 'jquery'], function (app, $) {
-    'use strict';
-    // use app here
-    console.log(app);
-    console.log('Running jQuery %s', $().jquery);
+  'use strict';
+  // use app here
+  console.log(app);
+  console.log('Running jQuery %s', $().jquery);
 });
