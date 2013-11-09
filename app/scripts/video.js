401ec724d47a97e6293a029d9ca635a206bb04b0
Davor Babic <davor@davor.se>
2013-11-09 15:15:19 +0100

    Now only displaying canvas with a video element

--- app/scripts/video.js
+++ app/scripts/video.js
@@ -4,8 +4,15 @@ define([
 ], function($,
             VideoTemplate) {
   'use strict';
-  var that = this;
   var videoElement, canvas, context;
+
+  function draw(width, height) {
+    if(videoElement.paused || videoElement.ended) {
+      return false;
+    }
+    context.drawImage(videoElement, 0, 0, width, height);
+    return setTimeout(draw, 20, width, height);
+  }
 
   return {
     initialize: function() {
@@ -16,22 +23,15 @@ define([
       canvas = document.getElementById('video-canvas');
       context = canvas.getContext('2d');
 
-      cw = Math.floor(canvas.clientWidth / 100);
-      ch = Math.floor(canvas.clientHeight / 100);
+      cw = Math.floor(canvas.clientWidth / 1);
+      ch = Math.floor(canvas.clientHeight / 1);
       canvas.width = cw;
       canvas.height = ch;
 
       videoElement.addEventListener('play', function() {
-        that.draw(cw, ch);
+        draw(cw, ch);
       });
-    },
-
-    draw: function(width, height) {
-      if(videoElement.paused || videoElement.ended) {
-        return false;
-      }
-      context.drawImage(videoElement, 0, 0, width, height);
-      return setTimeout(that.draw, 20, width, height);
+      videoElement.play();
     }
   };
 });
