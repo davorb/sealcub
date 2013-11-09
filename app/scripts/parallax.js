04b0b9190099bd2d79c95625923a3154a75789de
Dax <you@example.com>
2013-11-09 16:55:09 +0100

    sf

--- app/scripts/parallax.js
+++ app/scripts/parallax.js
@@ -1,14 +1,37 @@
-define([
-  'jquery',
-  'text!templates/parallaxTemplate.html'
-], function($,
-            ParallaxTemplate) {
+define(['jquery', 'text!templates/parallaxTemplate.html'], function($,
+    ParallaxTemplate) {
   'use strict';
   var that = this;
+  var window = $(window);
+  var yPos;
 
   return {
     initialize: function() {
       $('body').html(ParallaxTemplate);
+//      $('section[data-type="background"]').each(function() {
+//        var $bgobj = $(this); // assigning the object
+//        window.scroll(function() {
+//          yPos = -($window.scrollTop() / $bgobj.data('speed'));
+//          console.log(yPos);
+//        });
+//      });
+      
+      $('section[data-type="background"]').each(function(){
+        var $bgobj = $(this); // assigning the object
+     
+        window.scroll(function() {
+            var yPos = -($window.scrollTop() / $bgobj.data('speed'));
+             
+            // Put together our final background position
+            var coords = '50% '+ yPos + 'px';
+ 
+            // Move the background
+            $bgobj.css({ backgroundPosition: coords });
+            alert("fdhdf");
+        });
+    });    
+      
+
     }
   };
 });
