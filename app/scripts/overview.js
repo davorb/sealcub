4f6bfbc195a05176750d186159d00580d39573f0
Davor Babic <davor@davor.se>
2013-11-12 02:42:42 +0100

    Added teletype

--- app/scripts/overview.js
+++ app/scripts/overview.js
@@ -1,14 +1,21 @@
 define(
 [
   'jquery',
-  'text!templates/overviewTemplate.html'
-], function($, OverviewTemplate) {
+  'text!templates/overviewTemplate.html',
+  'teletype'
+], function($,
+            OverviewTemplate,
+            Teletype) {
   'use strict';
   return {
     initialize: function() {
       $('body').prepend(OverviewTemplate);
       $('#overview').css('height',
                          $(window).height());
+      $('#intro-text').teletype({
+        animDelay: 150,
+        text: 'Hi.'
+      });
     }
   };
 });
---
+++ app/scripts/teletype.js
@@ -0,0 +1,15 @@
+define(['jquery'], function($) {
+  $.fn.teletype = function(opts){
+    var $this = this,
+        defaults = {
+          animDelay: 50
+        },
+        settings = $.extend(defaults, opts);
+
+    $.each(settings.text.split(''), function(i, letter){
+      setTimeout(function(){
+        $this.html($this.html() + letter);
+      }, settings.animDelay * i * (Math.random() + 0.3));
+    });
+  };
+});
